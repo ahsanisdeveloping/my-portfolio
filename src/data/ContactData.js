80bf97db365f8be5fe0acc152449e18f2b57ad3d
Ahsan Usman <ahsanusmanofficial@gmail.com>
2024-02-03 22:10:34 +0500

    integrated contact module in my portfolio project

---
+++ src/data/ContactData.js
@@ -0,0 +1,27 @@
+const contactArray = [
+    {
+        title:"Github",
+        link:"https://github.com/ahsanisdeveloping"
+    },
+    {
+        title:"LinkedIn.",
+        link:"https://www.linkedin.com/in/ahsan-usman-5a1a6b270?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+    },
+    {
+        title:"Email",
+        link:"mailto:ahsanusman57@gmail.com"
+    },
+    {
+        title:"Facebook",
+        link:"https://www.facebook.com/profile.php?id=100010405968676&mibextid=ZbWKwL"
+    },
+    {
+        title:"Instagram",
+        link:"https://www.instagram.com/ahsxnnn?igsh=MTdvOGYxcDYzdGhobw=="
+    },
+    {
+        title:"X (twitter)",
+        link:"https://x.com/_SheikhZada_?t=hWnIAb1bFlsat1SLRm-tGQ&s=09"
+    }
+]
+export default contactArray;
--- src/screens/Contact.jsx
+++ src/screens/Contact.jsx
@@ -1,9 +1,18 @@
+import "../styles/contact.css";
+import contactArray from '../data/ContactData';
 const Contact = () => {
-    return ( 
-        <div>
-            <h1>I am Contact Us Component</h1>
-        </div>
-     );
-}
- 
+  return (
+    <div className="contact">
+      <h1 className="contactHeadingText">Contact</h1>
+      <div className="contactButtonsDiv">
+        {
+            contactArray.map((value)=>(
+                <button className="contactButton"><a className="contactButtonAnchor" href={value.link} target='_blank' rel="noreferrer">{value.title}</a></button>
+            ))
+        } 
+      </div>
+    </div>
+  );
+};
+
 export default Contact;

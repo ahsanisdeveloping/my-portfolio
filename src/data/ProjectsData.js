f0410df65dc2987717fb0854d471e56e08798e4d
Ahsan Usman <ahsanusmanofficial@gmail.com>
2024-02-03 20:57:01 +0500

    Integrated projects module in my portfolio website

---
+++ src/data/ProjectsData.js
@@ -0,0 +1,80 @@
+const projectsArray = [
+    {
+        title:"Text Utility App",
+        description:"A simple tool that empowers you to modify your text effortlessly.",
+        githubLink:"https://github.com/ahsanisdeveloping/TextUtils",
+        hostingLink:"hehehe",
+        languages:["React JS", "HTML", "CSS"]
+    },
+    {
+        title:"Explore Haven",
+        description:"A simple Kotlin-powered app transforming property rentals.",
+        githubLink:"https://github.com/ahsanisdeveloping/explore-haven",
+        hostingLink:"",
+        languages:["Kotlin","Firebase"]
+    },
+    {
+        title:"Primary Web UI",
+        description:"A sleek and elegant website UI - promoting minimalistic design. ",
+        githubLink:"https://github.com/ahsanisdeveloping/primary-webpage-ui",
+        hostingLink:"skdjald",
+        languages:["HTML", "CSS", "Javascript"]
+    },
+    {
+        title:"Form Validaiton",
+        description:"A simple form that shows the validation procedure in javascript.",
+        githubLink:"https://github.com/ahsanisdeveloping/ue-form",
+        hostingLink:"",
+        languages:["Javascript", "HTML", "CSS"]
+    },
+    {
+        title:"Vintage Kodak",
+        description:"A Website showcasing the history of photography and cameras.",
+        githubLink:"https://github.com/ahsanisdeveloping/vintage-kodak",
+        hostingLink:"",
+        languages:["HTML", "CSS"]
+    },
+    {
+        title:"LMS",
+        description:"A simple project about Library Management System using PHP as backend language.",
+        githubLink:"https://github.com/ahsanisdeveloping/library-management-system",
+        hostingLink:"",
+        languages:["PHP", "HTML", "CSS"]
+    },
+    {
+        title:"Calculator App",
+        description:"A simple calculator app designed using vanilla CSS and HTML.",
+        githubLink:"https://github.com/ahsanisdeveloping/calculator",
+        hostingLink:"",
+        languages:["HTML", "CSS"]
+    },
+    {
+        title:"PHP CRUD",
+        description:"A simple PHP Web app designed for implement SQL CRUD operations.",
+        githubLink:"https://github.com/ahsanisdeveloping/crud-php",
+        hostingLink:"",
+        languages:["PHP","MySQL", "HTML", "CSS"]
+    },
+    {
+        title:"SASSIO Web UI",
+        description:"A sleek and elegant website UI - promoting minimalistic design.",
+        githubLink:"https://github.com/ahsanisdeveloping/sassio-webpage-ui",
+        hostingLink:"",
+        languages:[ "HTML", "CSS"]
+    },
+    {
+        title:"Zain Portfolio",
+        description:"A portfolio Webpage UI designed for a client using vanilla CSS.",
+        githubLink:"https://github.com/ahsanisdeveloping/zain-portfolio-client",
+        hostingLink:"",
+        languages:["HTML", "HTML", "Javascript"]
+    },
+    {
+        title:"Average Calculator",
+        description:"A simple average calculator app designed to implement creative design using vanilla CSS.",
+        githubLink:"https://github.com/ahsanisdeveloping/average-calculator",
+        hostingLink:"",
+        languages:[ "HTML", "CSS"]
+    }
+];
+export default projectsArray;
--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -1,7 +1,49 @@
+import "../styles/projects.css";
+import projectsArray from "../data/ProjectsData";
 const Projects = () => {
-    return ( 
-        <div><h1>I am Projects Component</h1></div>
-     );
-}
- 
+  return (
+    <div className="projects">
+      <div className="projectsMain">
+        <h1 className="projectsHeading">Projects</h1>
+        <div className="projectsDiv">
+          {projectsArray.map((value) => (
+            <div class="e-card playing project">
+              <div class="image"></div>
+              <div class="wave"></div>
+              <div class="wave"></div>
+              <div class="wave"></div>
+              <div className="meriDiv">
+              <div className="projectTitleDiv">
+                <h1 className="projectTitleText">{value.title}</h1>
+              </div>
+              <div className="projectDescriptionDiv">
+                <p className="projectDescriptionText">{value.description}</p>
+              </div>
+              <div className="projectButtonsDiv">
+                <button className="projectGithubButton">
+                  <a href={value.githubLink} className="projectGithubButtonAnchor" target='_blank' rel="noreferrer">
+                    Github
+                  </a>
+                </button>
+                {value.hostingLink!=="" && <button className="projectHostingButton">
+                  <a href={value.hostingLink} className="projectHostingButtonAnchor" target='_blank' rel="noreferrer">
+                    Hosting
+                  </a>
+                </button>}
+              </div>
+              <div className="languagesDiv">
+              {value.languages.map((arrayValue)=>(
+                    <span className="language">{arrayValue}</span>
+                ))}
+                </div>
+              
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Projects;
